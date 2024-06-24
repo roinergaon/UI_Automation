bb2e48f79aa8426e73c391c37f42754e6967dffc
RNergaon <roing65@gmail.com>
2024-06-24 11:37:51 +0300

    Add tests cases for the login

--- tests/test_loginPage.py
+++ tests/test_loginPage.py
@@ -8,30 +8,103 @@ from pageObjects.products_page import ProductsPage
 from utils.base_test import BaseClass
 
 class TestLoginPage(BaseClass):
+    @allure.description("Verify that user can't log in with invalid username")
+    @allure.title("Invalid username test")
+    @allure.feature("Login tests")
+    @allure.tag("Login tests")
     def test_tc_01(self):
-            login_page = LoginPage(self.driver)
-            with allure.step("Enter user name"):
-                user_name = self.get_test_case_data('test_case_1', 'username')
-            with allure.step("Enter password"):
-                password = self.get_test_case_data('test_case_1', 'password')
-            with allure.step("Press login"):
-                login_page.login(user_name, password)
-            product_page = ProductsPage(self.driver)
-            title = login_page.get_text(product_page.title)
-            test_case_data = self.get_test_case_data('test_case_1', 'expected_result')
-            assert test_case_data == title
-
-    def test_tc_02(self):
         login_page = LoginPage(self.driver)
         with allure.step("Enter invalid user name"):
-            user_name = self.get_test_case_data('test_case_2', 'username')
+            user_name = self.get_test_case_data('login_page_test_data','test_case_1', 'username')
         with allure.step("Enter password"):
-            password = self.get_test_case_data('test_case_2', 'password')
+            password = self.get_test_case_data('login_page_test_data','test_case_1', 'password')
         with allure.step("Press login"):
             login_page.login(user_name, password)
         error_message = login_page.get_text(login_page.error_label)
-        test_case_data = self.get_test_case_data('test_case_2', 'expected_result')
+        test_case_data = self.get_test_case_data('login_page_test_data','test_case_1', 'expected_result')
         assert test_case_data in error_message
 
+    @allure.description("Verify that user can't log in with invalid password")
+    @allure.title("Invalid password test")
+    @allure.feature("Login tests")
+    @allure.tag("Login tests")
+    def test_tc_02(self):
+        log = self.get_logger()
+        log.info("")
+        login_page = LoginPage(self.driver)
+        with allure.step("Enter user name"):
+            user_name = self.get_test_case_data('login_page_test_data', 'test_case_2', 'username')
+        with allure.step("Enter password"):
+            password = self.get_test_case_data('login_page_test_data', 'test_case_2', 'password')
+        with allure.step("Press login"):
+            login_page.login(user_name, password)
+        error_message = login_page.get_text(login_page.error_label)
+        test_case_data = self.get_test_case_data('login_page_test_data', 'test_case_1', 'expected_result')
+        assert test_case_data in error_message
+
+    @allure.description("Verify that user can't log in with empty username field")
+    @allure.title("Empty username test")
+    @allure.feature("Login tests")
+    @allure.tag("Login tests")
+    def test_tc_03(self):
+        log = self.get_logger()
+        log.info("Verify login functionality, check valid user name and password")
+        login_page = LoginPage(self.driver)
+        with allure.step("Enter user name"):
+            user_name = self.get_test_case_data('login_page_test_data', 'test_case_3', 'username')
+        with allure.step("Enter password"):
+            password = self.get_test_case_data('login_page_test_data', 'test_case_3', 'password')
+        with allure.step("Press login"):
+            login_page.login(user_name, password)
+        error_message = login_page.get_text(login_page.error_label)
+        test_case_data = self.get_test_case_data('login_page_test_data', 'test_case_3', 'expected_result')
+        assert test_case_data in error_message
+    @allure.description("Verify that user can't log in with empty password field")
+    @allure.title("Empty password test")
+    @allure.feature("Login tests")
+    @allure.tag("Login tests")
+    def test_tc_04(self):
+        login_page = LoginPage(self.driver)
+        with allure.step("Enter user name"):
+            user_name = self.get_test_case_data('login_page_test_data', 'test_case_4', 'username')
+        with allure.step("Enter password"):
+            password = self.get_test_case_data('login_page_test_data', 'test_case_4', 'password')
+        with allure.step("Press login"):
+            login_page.login(user_name, password)
+        error_message = login_page.get_text(login_page.error_label)
+        test_case_data = self.get_test_case_data('login_page_test_data', 'test_case_4', 'expected_result')
+        assert test_case_data in error_message
+    @allure.description("Verify that user can't log in with empty username and empty password fields")
+    @allure.title("Empty username and password test")
+    @allure.feature("Login tests")
+    @allure.tag("Login tests")
+    def test_tc_05(self):
+        login_page = LoginPage(self.driver)
+        with allure.step("Enter user name"):
+            user_name = self.get_test_case_data('login_page_test_data', 'test_case_5', 'username')
+        with allure.step("Enter password"):
+            password = self.get_test_case_data('login_page_test_data', 'test_case_5', 'password')
+        with allure.step("Press login"):
+            login_page.login(user_name, password)
+        error_message = login_page.get_text(login_page.error_label)
+        test_case_data = self.get_test_case_data('login_page_test_data', 'test_case_5', 'expected_result')
+        assert test_case_data in error_message
+
+    @allure.description("Verify that user can log in with valid username and password")
+    @allure.title("Valid username test")
+    @allure.feature("Login tests")
+    @allure.tag("Login tests")
+    def test_tc_06(self):
+        login_page = LoginPage(self.driver)
+        with allure.step("Enter user name"):
+            user_name = self.get_test_case_data('login_page_test_data', 'test_case_6', 'username')
+        with allure.step("Enter password"):
+            password = self.get_test_case_data('login_page_test_data', 'test_case_6', 'password')
+        with allure.step("Press login"):
+            login_page.login(user_name, password)
+        product_page = ProductsPage(self.driver)
+        title = login_page.get_text(product_page.title)
+        test_case_data = self.get_test_case_data('login_page_test_data', 'test_case_6', 'expected_result')
+        assert test_case_data == title
 
 
--- utils/base_test.py
+++ utils/base_test.py
@@ -1,15 +1,33 @@
 import json
+import logging
 import os
-import sys
-
 import pytest
-#sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 @pytest.mark.usefixtures("setup")
 class BaseClass:
-    def get_test_case_data(self, test_case, expected_result):
+
+    def get_logger(self):
+        logger = logging.getLogger('UI Automate testing')
+        logger.setLevel(logging.INFO)  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+
+        # Create a file handler that overwrites the log file each time
+        file_handler = logging.FileHandler("log_info.log", mode='w')
+        file_handler.setLevel(logging.INFO)
+
+        # Create a logging format
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        file_handler.setFormatter(formatter)
+
+        if logger.hasHandlers():
+            logger.handlers.clear()
+
+        # Add the file handler to the logger
+        logger.addHandler(file_handler)
+        return logger
+
+    def get_test_case_data(self, tested_page, test_case, expected_result):
         script_directory = os.path.dirname(os.path.abspath(__file__))
-        data_path = os.path.join(script_directory, 'test_data.json')
+        data_path = os.path.join(script_directory, f"tests data/{tested_page}.json")
         with open(data_path, 'r') as file:
             test_data = json.load(file)
 
